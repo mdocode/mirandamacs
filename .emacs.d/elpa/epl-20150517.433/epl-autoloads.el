a738824b0421f5b19511a841cf7af67829745d78
Miranda Dobbs <mdobbs09@gmail.com>
2015-07-02 16:43:00 -0400

    add elpa libraries

---
+++ .emacs.d/elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (21909 39646 593014 148000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
